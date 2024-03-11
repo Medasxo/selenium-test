3cc6920bfbf7e84f660c2a35d622c8c3cfa70b06
Medasxo <70206510+Medasxo@users.noreply.github.com>
2024-03-11 12:21:26 +0200

    Update TestTask4.java

--- src/test/java/TestTask4.java
+++ src/test/java/TestTask4.java
@@ -31,10 +31,10 @@ public class TestTask4 {
   
 	@BeforeClass
 	public static void setUp() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
 		WebDriverManager.chromedriver().setup();
 		userCreationDriver = new ChromeDriver(options);
 		createUser();
